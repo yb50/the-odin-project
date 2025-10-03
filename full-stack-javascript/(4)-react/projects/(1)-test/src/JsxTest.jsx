351794a873cd1053fb1b1ccefac0b9be5696c1c7
Yusuf Bakay <110858019+yb50@users.noreply.github.com>
2025-10-03 21:04:12 +0900

    feat: add first jsx practice
    - Created a function called 'JsxTest' to practice jsx for the first time.

---
+++ full-stack-javascript/(4)-react/projects/(1)-test/src/JsxTest.jsx
@@ -0,0 +1,9 @@
+function JsxTest() {
+  return (
+    <>
+      <h2>This is my first attempt at writing text through jsx!</h2>
+    </>
+  )
+}
+
+export default JsxTest;
--- full-stack-javascript/(4)-react/projects/(1)-test/src/main.jsx
+++ full-stack-javascript/(4)-react/projects/(1)-test/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Greeting from './Greeting.jsx'
+import JsxTest from './JsxTest.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Greeting />
+    <JsxTest />
   </StrictMode>,
 )
